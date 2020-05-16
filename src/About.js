253e4e7825a075d2dac871a0cde87103074467c2
007atulpandey <apandey0826@gmail.com>
2020-05-16 17:36:45 +0530

    new data

--- src/About.js
+++ src/About.js
@@ -1,13 +1,106 @@
 import React from 'react';
 
 import './App.css';
+import axios from 'axios'
+class  About extends React.Component {
+   
+   constructor(){
+     super()
+     this.state={
+       div:undefined,
+       data:'',
+       datas:[<div> WHOLE DATA   </div>],
+       hide :""
+     }
 
-function About() {
+     this.click = this.click.bind(this)
+   }
+
+   componentDidMount(){
+    //  var loop =[{"name":"hello "},{name:" Atul"}]
+    //  this.setState({
+    //   div: loop.map((lp)=><div> {lp.name} </div>)
+
+    //  })
+
+     axios.get('https://api.covid19india.org/state_district_wise.json')
+        .then((response)=>
+           
+            this.setState({
+                div :response,
+               
+                        })
+                
+        )
+        .catch(error=>
+            console.log(error)
+        )
+        console.log(this.state.div)
+   }
+  click (){
+    this.setState({
+      hide:"hide"
+    })
+    this.setState(prevState => ({
+      datas: [
+          ...prevState.datas, 
+          <div> ATUL PANDEY 0826 </div>
+      ]
+  }))
+    console.log(this.state.div)
+    // this.setState({
+    //   data:this.state.div.data.map((dis)=>{
+    //     cons
+    //   })
+    // })
+    const check =this.state.div;
+    
+    console.log(check.data)
+    Object.keys(check.data).map((data)=>{
+      console.log(data)
+      this.setState(prevState => ({
+        datas: [
+            ...prevState.datas, 
+            <div style={{"color":"green"}}> <h3> {data}</h3> </div>
+        ]
+    }))
+     console.log(check.data[data])
+
+
+    Object.keys(check.data[data].districtData).map((keys)=>{
+      console.log(keys);
+      this.setState(prevState => ({
+        datas: [
+            ...prevState.datas, 
+            <div style={{"color":"red"}}> {keys} </div>,
+            <div style={{"color":"blue" ,"display":"flex","flexDirection":"row","textAlign" :"center","justifyContent" :"center","alignItems":"center"}}> 
+            <div > Active {check.data[data].districtData[keys].active}  </div>
+            <div > Recover {check.data[data].districtData[keys].recovered}  </div>
+            <div > Confirm {check.data[data].districtData[keys].confirmed}  </div>
+            <div > Death {check.data[data].districtData[keys].deceased}  </div>
+         </div>
+        ]
+    }))
+    })
+
+
+    })
+  //  check.map((dis)=>{
+  //         console.log(dis)
+  //   })
+  }
+
+
+  render(){
   return (
     <div className="App">
       <h1>About Page</h1>
+      <button className={this.state.hide} onClick={this.click} > clickme </button>
+      {this.state.datas}
     </div>
   );
+}
+
 }
 
 export default About;
--- src/Shop.js
+++ src/Shop.js
@@ -12,7 +12,11 @@ class Shop extends React.Component {
            dead:0,
            recover:0,
            city:"",
-           confirm:0
+           confirm:0,
+           hide:"hide",
+           hidecity:"hide",
+           datas:[<div> WHOLE DATA THROUGHOUT  </div>],
+           toggle:""
 
 
        }
@@ -22,6 +26,7 @@ class Shop extends React.Component {
        this.handle = this.handle.bind(this)
        this.allele = this.allele.bind(this)
        this.show= this.show.bind(this)
+       this.city= this.city.bind(this)
    }
   
     componentDidMount(){
@@ -46,32 +51,75 @@ class Shop extends React.Component {
         })
         }
     allele(res){
+        this.setState({
+            hidecity:"",
+            toggle:"hide"
+        })
        console.log("jelli")
-       this.setState({
+       if(this.state.city==""){
+           console.log("err")
+       }
+       else{       this.setState({
         active:res.districtData[this.state.city].active,
         dead:res.districtData[this.state.city].deceased,
         recover:res.districtData[this.state.city].recovered,
         confirm:res.districtData[this.state.city].confirmed
        
+       
     })
-    console.log(res.districtData[this.state.city])
+}
+ 
        
     }
     show(event){
+        
         const state = this.state.stateName;
         
         console.log(this.state.stateName)
        const loop =this.state.data[state];
     //    console.log(this.state.data[state].districtData[this.state.city].active)
        if(this.state.data[state]){
-       
-       this.allele(this.state.data[state])}
+           setTimeout(()=>{
+            Object.keys(this.state.data[state].districtData).map((keys)=>{
+                console.log(keys);
+                this.setState(prevState => ({
+                  datas: [
+                      ...prevState.datas, 
+                      <div style={{"color":"red"}}> {keys} </div>,
+                      <div style={{"color":"blue" ,"display":"flex","flexDirection":"row","textAlign" :"center","justifyContent" :"center","alignItems":"center"}}> 
+                      <div > Active {this.state.data[state].districtData[keys].active}  </div>
+                      <div > Recover {this.state.data[state].districtData[keys].recovered}  </div>
+                      <div > Confirm {this.state.data[state].districtData[keys].confirmed}  </div>
+                      <div > Death {this.state.data[state].districtData[keys].deceased}  </div>
+                   </div>
+                  ]
+              }))
+              })
+
+              this.setState({
+                hide:"",
+                toggle:"",
+                hidecity:"hide"
+            })
+            if(this.state.city)
+           this.allele(this.state.data[state])
+           },1000)
+        
+      
+         }
 
  
            console.log("errr")
        
 
     }
+city(){
+    
+    if(this.state.city){
+        this.show();
+    }
+}
+
     render(){
        
   
@@ -89,9 +137,15 @@ class Shop extends React.Component {
         </span>
         <br/>
     <input placeholder="State Name" type="text" onChange={this.handleEvent} value = {this.state.stateName} />
-    
-    <input placeholder="City"  type="text" onChange={this.handle} value = {this.state.city} />
     <button onClick={this.show} > Click me </button>
+    <div className={this.state.hide}>
+    <input placeholder="City"  type="text" onChange={this.handle} value = {this.state.city} />
+    <button onClick={this.city} > Click me </button>
+    </div>
+    <div className={this.state.toggle}>
+    {this.state.datas}
+   </div> <div className={this.state.hidecity}>
+    
     <div>active :{
         this.state.active
     }
@@ -105,6 +159,7 @@ class Shop extends React.Component {
     </div>
     <div>confirm :
         {this.state.confirm}
+    </div>
     </div>
     </div>
   );
